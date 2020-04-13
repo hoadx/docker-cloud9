cd40e8dd57687f38ecf44d55c92d3ec62d3ffc7a
Hoa Duong <duongxuanhoa@gmail.com>
2020-04-13 19:19:08 +0700

    Fix shell command

--- usermode/docker-entrypoint.sh
+++ usermode/docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 WORKSPACE=${WORKSPACE:-/workspace}
 CHANGE_OWNER=${CHANGE_OWNER:-0}
 
-if [ \("$USERID" > 0\) -o \("$GROUPID" > 0\) ]
+if [ "$USERID" -gt 0 ] || [ "$GROUPID" -gt 0 ]
 then                  
         ORIGPASSWD=$(cat /etc/passwd | grep c9)
         ORIG_UID=$(echo $ORIGPASSWD | cut -f3 -d:)
         ORIG_GID=$(echo $ORIGPASSWD | cut -f4 -d:)
                                                                                 
-        if [ \("$USERID" != "$ORIG_UID"\) -o \("$GROUPID" != "$ORIG_GID"\) ]; then
+        if [ "$USERID" != "$ORIG_UID" ] || [ "$GROUPID" != "$ORIG_GID" ]; then
                 sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$USERID:$GROUPID:/" /etc/passwd
                 sed -i -e "s/c9:x:$ORIG_GID:/c9:x:$GROUPID:/" /etc/group          
                 ORIG_HOME=$(echo $ORIGPASSWD | cut -f6 -d:)                
